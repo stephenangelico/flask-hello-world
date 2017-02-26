7b7c4984fb7b188820369c5ad77b4da20086f39e
Stephen Angelico <raaff35lightning@gmail.com>
2017-02-27 00:45:57 +1100

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from os import environ
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/hello")
+def say_hi():
+	return "Hello World!"
+
+if __name == "__main__":
+	app.run(host=environ['IP'],
+		port=int(environ['PORT']))
